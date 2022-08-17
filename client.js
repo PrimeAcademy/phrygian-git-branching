497bf2eb18648d7b649cf3f0b7ab046fbb0afc9a
Oscar Carbajal <Oscar.carbajal.work@gmail.com>
2022-08-16 19:43:52 -0500

    added ul tag to append in function addStudent

--- client.js
+++ client.js
@@ -6,3 +6,15 @@ function readyNow() {
 }
 
 // Function addStudent() adds an li to #student-list - Oscar
+
+function addStudent() {
+    console.log('in addStudent');
+    let newStudent = $('#add-student').val();
+    console.log('newStudent', newStudent);
+    $('#student-list').append(`
+        <ul>
+        <li>${newStudent}</li>
+        </ul>
+        `
+    );
+}
